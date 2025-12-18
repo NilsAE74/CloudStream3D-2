0401d9658ede95117d00c400b88a1fea88b9eb04
NilsAE74 <130651138+NilsAE74@users.noreply.github.com>
2025-12-18 11:20:41 +0100

    Add grid and axis toggle functionality to ToolBar

--- client/src/components/ToolBar/ToolBar.js
+++ client/src/components/ToolBar/ToolBar.js
@@ -11,7 +11,10 @@ import {
   Refresh,
   Straighten,
   Palette,
-  Layers
+  Layers,
+  GridOn,
+  GridOff,
+  ThreeDRotation
 } from '@mui/icons-material';
 
 function ToolBar({ 
@@ -19,13 +22,17 @@ function ToolBar({
   toggleMeasurementMode, 
   colorMode, 
   setColorMode,
-  onResetView
+  onResetView,
+  showGrid,
+  toggleGrid,
+  showAxis,
+  toggleAxis
 }) {
   return (
     <Paper
       sx={{
-        position: 'absolute',
-        top: 10,
+        position:  'absolute',
+        top:  10,
         left: '50%',
         transform: 'translateX(-50%)',
         display: 'flex',
@@ -69,8 +76,30 @@ function ToolBar({
 
       <Divider orientation="vertical" flexItem />
 
+      {/* Grid Toggle */}
+      <Tooltip title={showGrid ? "Hide Grid" : "Show Grid"}>
+        <IconButton 
+          onClick={toggleGrid}
+          color={showGrid ? "primary" : "default"}
+        >
+          {showGrid ? <GridOn /> : <GridOff />}
+        </IconButton>
+      </Tooltip>
+
+      {/* Axis Toggle */}
+      <Tooltip title={showAxis ? "Hide Axis" : "Show Axis"}>
+        <IconButton 
+          onClick={toggleAxis}
+          color={showAxis ? "primary" : "default"}
+        >
+          <ThreeDRotation />
+        </IconButton>
+      </Tooltip>
+
+      <Divider orientation="vertical" flexItem />
+
       {/* Measurement Tool */}
-      <Tooltip title={measurementMode ? "Disable Measurement" : "Enable Measurement"}>
+      <Tooltip title={measurementMode ?  "Disable Measurement" : "Enable Measurement"}>
         <IconButton 
           onClick={toggleMeasurementMode}
           color={measurementMode ? "secondary" : "default"}
